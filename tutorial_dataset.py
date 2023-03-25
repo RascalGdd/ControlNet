50cfd783a663ab5f4da2a669f43be83373ff91b4
Diandian Guo <guodiandian1998@163.com>
2023-03-25 16:35:24 +0100

    update dataset, add mask

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from torch.utils.data import Dataset
 
+
 json_path = r'./datasets/camo_diff/testjson_dict.json'
 size = 512
 
@@ -22,11 +23,13 @@ class MyDataset(Dataset):
 
         source_filename = item['source']
         target_filename = item['target']
+        mask_filename = item['target'].replace("target", "mask").replace("jpg", "png")
         prompt = item['prompt']
 
 
         source = cv2.imread(os.path.join(r"./datasets", source_filename.replace("\\", "/")))
         target = cv2.imread(os.path.join(r"./datasets", target_filename.replace("\\", "/")))
+        mask = cv2.imread(os.path.join(r"./datasets", mask_filename.replace("\\", "/")), 0)
 
         # Do not forget that OpenCV read images in BGR order.
         source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
@@ -35,14 +38,20 @@ class MyDataset(Dataset):
         # resize the image
         source = cv2.resize(source, (size, size))
         target = cv2.resize(target, (size, size))
+        mask = cv2.resize(mask, (size, size))
+
 
         # Normalize source images to [0, 1].
-        source = source.astype(np.float32) / 255.0
+        source = (source.astype(np.float32) / 127.5) - 1.0
 
-        # Normalize target images to [-1, 1].
+        # Normalize target images to [0, 1].
         target = (target.astype(np.float32) / 127.5) - 1.0
 
-        return dict(jpg=target, txt=prompt, hint=source)
+        # Normalize mask images to [0, 1].
+        mask = mask.astype(np.float32) / 255.0
+        mask = np.expand_dims(mask, 2)
+
+        return dict(jpg=target, txt=prompt, hint=source, mask=mask)
 
 dataset = MyDataset()
 print(len(dataset))
@@ -51,6 +60,8 @@ item = dataset[1234]
 jpg = item['jpg']
 txt = item['txt']
 hint = item['hint']
+mask = item['mask']
 print(txt)
 print(jpg.shape)
 print(hint.shape)
+print(mask.shape)
