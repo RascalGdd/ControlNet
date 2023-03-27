73edda3eae0b767ebef975bec0e07a38fd667a61
Diandian Guo <guodiandian1998@163.com>
2023-03-27 11:16:56 +0200

    debug

--- cldm/cldm.py
+++ cldm/cldm.py
@@ -420,7 +420,7 @@ class ControlLDM(LatentDiffusion):
         if unconditional_guidance_scale > 1.0:
             uc_cross = self.get_unconditional_conditioning(N)
             uc_cat = c_cat  # torch.zeros_like(c_cat)
-            uc_full = {"c_concat": [uc_cat], "c_crossattn": [uc_cross]}
+            uc_full = {"c_concat": [uc_cat], "c_crossattn": [uc_cross], "c_concat_mask": [c_concat_mask]}
             samples_cfg, _ = self.sample_log(cond={"c_concat": [c_cat], "c_crossattn": [c], "c_concat_mask": [c_concat_mask]},
                                              batch_size=N, ddim=use_ddim,
                                              ddim_steps=ddim_steps, eta=ddim_eta,
